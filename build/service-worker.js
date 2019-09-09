4fe64c91368c00a44466b6f40aeee8900ea5f96d
Miroslaw <m5b@ukr.net>
2019-09-09 09:42:45 +0300

    add new code

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aca956d57e11ef7d6aeae8b79332b16a.js"
+  "/precache-manifest.3c0ba8fbe46c44110ecfb886c0387d7b.js"
 );
 
 self.addEventListener('message', (event) => {
